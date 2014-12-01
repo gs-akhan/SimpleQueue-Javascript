06895f6d352dc355eb04c2ff2793c0927263ac17
gs-akhan <akhan@gainsight.com>
2014-12-02 01:01:55 +0530

    Added add(), drain() and getItems()

---
+++ queue.js
@@ -0,0 +1,48 @@
+function Queue() {
+	
+	var first, last, item;
+
+	function Item(fn) {
+		this.fn = fn;
+		this.next = void 0;
+	}
+
+
+	return {
+		add : function(fn) {
+			 item = new Item(fn);
+			 
+			 if(last) {
+			 	last.next = item; 
+			 }
+
+			 else {
+			 	first = item;
+			 }
+
+			 last = item;
+			 item = void 0;
+
+
+		},	
+
+		drain : function() {
+			var f = first;  
+
+			while(f) {
+				f.fn();
+				f = f.next;
+			}
+
+			first = last = item = null;
+
+		},
+
+		getItems : function() {
+			return first;
+		}
+
+
+	}
+
+}
